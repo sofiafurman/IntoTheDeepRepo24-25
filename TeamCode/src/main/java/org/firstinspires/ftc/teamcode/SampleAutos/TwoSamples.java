62cb0017f8e0b7e75bb8a327920835509e31e63a
sofiafurman <sfurman180@gmail.com>
2024-11-12 15:28:56 -0700

    some vague semblance of an auto program... need to retune

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SampleAutos/TwoSamples.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SampleAutos/TwoSamples.java
@@ -10,38 +10,60 @@ import com.acmerobotics.roadrunner.Pose2d;
 import com.acmerobotics.roadrunner.SequentialAction;
 import com.acmerobotics.roadrunner.Vector2d;
 import com.acmerobotics.roadrunner.ftc.Actions;
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.acmerobotics.roadrunner.ftc.Actions;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 // Non-RR imports
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import org.firstinspires.ftc.teamcode.MecanumDrive;
 
+
 @Config
-@Autonomous(name = "SampleAutoBR", group = "Autonomous")
+@Autonomous(name = "TwoSamples", group = "Autonomous")
 //Next to red net zone. Once completed, should grab the two samples closest to the exit doors
 public class TwoSamples extends LinearOpMode {
-
+    private DcMotor leftFrontDrive  = null;
+    private DcMotor leftBackDrive   = null;
+    private DcMotor rightFrontDrive = null;
+    private DcMotor rightBackDrive  = null;
     //variable
 
 
     @Override
     public void runOpMode() throws InterruptedException {
         //instantiation
-        Pose2d initialPose = new Pose2d(0, 24, Math.PI / 2);
-        MecanumDrive drive = new MecanumDrive(hardwareMap, initialPose);
+        leftFrontDrive  = hardwareMap.get(DcMotor.class, "left_front_drive");
+        leftBackDrive   = hardwareMap.get(DcMotor.class, "left_back_drive");
+        rightFrontDrive = hardwareMap.get(DcMotor.class, "right_front_drive");
+        rightBackDrive  = hardwareMap.get(DcMotor.class, "right_back_drive");
+
+
+
+        Pose2d beginPose = new Pose2d(0, 24, 0);
+        MecanumDrive drive = new MecanumDrive(hardwareMap, beginPose);
 
 
         waitForStart();
 
+        leftFrontDrive.setPower(0.3);
+        leftBackDrive.setPower(0.3);
+        rightFrontDrive.setPower(0.3);
+        rightBackDrive.setPower(0.3);
+
         Actions.runBlocking(
-                drive.actionBuilder(initialPose)
-                        .lineToX(50)
-                        .lineToY(4)
-                        .lineToX(5)
-                        .lineToY(100)
+                drive.actionBuilder(beginPose)
+                        .lineToX(48)
+                        .setTangent(Math.PI / 2)
+                        .lineToY(-4)
+                        //.lineToX(5)
+                       // .lineToY(100)
                         .build());
     }
 }
