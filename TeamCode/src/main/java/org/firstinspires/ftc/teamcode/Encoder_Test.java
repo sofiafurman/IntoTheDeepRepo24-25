6aa7f3c3dc453336fc9a62b2026841f9e21545fa
payma18 <payma18@libertycommon.org>
2024-11-16 14:34:19 -0700

    full teleop better better!

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Encoder_Test.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Encoder_Test.java
@@ -10,7 +10,8 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 public class Encoder_Test extends LinearOpMode {
 
     private ElapsedTime runtime = new ElapsedTime();
-
+    boolean C_INPUT = false, PREV_C_INPUT, CONTROL = false;
+    int motorState = 0;
     private DcMotor motor;
 
     @Override
@@ -19,6 +20,9 @@ public class Encoder_Test extends LinearOpMode {
         motor = hardwareMap.get(DcMotor.class, "motor");
         motor.setDirection(DcMotor.Direction.FORWARD);
         motor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        motor.setTargetPosition(0);
+        motor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
 
         telemetry.addData("Status", "Initialized");
         telemetry.update();
@@ -26,10 +30,39 @@ public class Encoder_Test extends LinearOpMode {
         runtime.reset();
 
         while (opModeIsActive()) {
-            telemetry.addData("runtime ", runtime);
-            telemetry.addData("motorPos", motor.getCurrentPosition());
-            telemetry.update();
 
+            C_INPUT = gamepad1.a;
+
+            if (C_INPUT && !PREV_C_INPUT) {
+                switch (motorState) {
+                    case 0:
+                        motorState = 1;
+                        motor.setTargetPosition(0);
+                        motor.setPower(0.5);
+                        while (motor.isBusy()) {}
+                        motor.setPower(0.0);
+                        sleep(1000);
+                        break;
+                    case 1:
+                        motorState = 2;
+                        motor.setTargetPosition(100);
+                        motor.setPower(0.5);
+                        while (motor.isBusy()) {}
+                        motor.setPower(0.0);
+                        sleep(1000);
+                        break;
+                    case 2:
+                        motorState = 0;
+                        motor.setTargetPosition(200);
+                        motor.setPower(0.5);
+                        while (motor.isBusy()) {}
+                        motor.setPower(0.0);
+                        sleep(1000);
+                        break;
+                }
+            }
+
+            /*
             motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
             motor.setTargetPosition(1000);
             motor.setPower(1.0);
@@ -44,6 +77,17 @@ public class Encoder_Test extends LinearOpMode {
             while (motor.isBusy()) {}
             motor.setPower(0.0);
             sleep(1000);
+            */
+
+            telemetry.addData("runtime ", runtime);
+            telemetry.addData("motorPos", motor.getCurrentPosition());
+            telemetry.addData("C_INPUT", C_INPUT);
+            telemetry.addData("PREV_C_INPUT", PREV_C_INPUT);
+            telemetry.addData("CONTROL", CONTROL);
+            telemetry.update();
+
+            PREV_C_INPUT = C_INPUT;
+            CONTROL = !CONTROL;
         }
     }
 }
