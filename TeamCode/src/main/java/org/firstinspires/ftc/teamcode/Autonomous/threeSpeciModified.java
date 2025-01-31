bc1edc00e6b9996856760d668be5d3bcb4e544eb
sofiafurman <sfurman180@gmail.com>
2025-01-31 07:27:30 -0700

    more 4speci

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/threeSpeciModified.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/threeSpeciModified.java
@@ -24,11 +24,11 @@ import com.qualcomm.robotcore.hardware.Servo;
 
 //this is theoretical if everything is perfectly tuned, but using 90 degrees and 24 inches
 @Config
-@Autonomous(name = "3 specimen", group = "Autonomous")
+@Autonomous(name = "DO NOT USE!!!!!!!!!!!!!! 3 specimen", group = "Autonomous")
 //Next to red net zone. Once completed, should score one sample to the low basket and drive to the end zone
 //Intake is on the front of the robot. Assume the low basket is at 45 degrees
 //MUCH OF THIS, ESPECIALLY INTAKE AND OUTTAKE, IS THEORETICAL!!! INTAKE AND OUTTAKE HAVEN'T BEEN IMPLEMENTED AS SUBROUTINES AT TIME OF WRITINGedge
-public class threeSpeci extends LinearOpMode{
+public class threeSpeciModified extends LinearOpMode{
 
     // if odometry is not properly tuned or constantly being retuned:
     // you MIGHT find it useful to change these values and use multiples of them instead of direct number
@@ -295,68 +295,38 @@ public class threeSpeci extends LinearOpMode{
                 .strafeToConstantHeading(new Vector2d(0, -50)); //28 for 2, 1  for 14?
 
         TrajectoryActionBuilder push3 = drive.actionBuilder(backingPose)
-                // GUI: KISS - 1 spec 3 push NH
-
-                // NOTE:
-                    // this will not actually work since heading stays fixed
-                    // use for major path changes only
-                    // do not use for minor path changes b/c turning may cause... unwanted collisions
-
                 // PART 1: score 1st spec & prepare
-                //.splineToConstantHeading(new Vector2d(0, -41), Math.toRadians(90)) //2 // 1 spec put this back in
-                //.splineToConstantHeading(new Vector2d(0, -50), Math.toRadians(90)) //3 // move back to avoid sub
-                //.splineToConstantHeading(new Vector2d(30, -40), Math.toRadians(90)) //4 // move to samp area
-                //.splineToConstantHeading(new Vector2d(30, -24), Math.toRadians(90)) //5 // move to be in line with samps
-
                 .splineToLinearHeading(new Pose2d(30, -40, Math.toRadians(90)), Math.toRadians(90))
                 .splineToConstantHeading(new Vector2d(30, -26), Math.toRadians(90))
-
                 //PART 2a: 1st push
-                .splineToConstantHeading(new Vector2d(39, -26), Math.toRadians(270), new TranslationalVelConstraint(20), new ProfileAccelConstraint(-10,10)) //6 // position
-                .splineToConstantHeading(new Vector2d(39, -57), Math.toRadians(90)) //7 // push // y originally -67
+                .splineToConstantHeading(new Vector2d(39, -26), Math.toRadians(270)) //6 // position
+                .splineToConstantHeading(new Vector2d(39, -57), Math.toRadians(90)) //7 // push
                 .splineToConstantHeading(new Vector2d(39, -26), Math.toRadians(90)) //8 // back to samp area
-
                 //PART 2b: 2nd push
-                .splineToConstantHeading(new Vector2d(50, -26), Math.toRadians(270), new TranslationalVelConstraint(20), new ProfileAccelConstraint(-10,10)) //9 // position
-                .splineToConstantHeading(new Vector2d(50, -57), Math.toRadians(270))//, new TranslationalVelConstraint(30), new ProfileAccelConstraint(-10,10)) //10 // push //TODO: change
-
-                //.splineToConstantHeading(new Vector2d(48, -26), Math.toRadians(90)) //11 // back to samp area
-
+                .splineToConstantHeading(new Vector2d(50, -26), Math.toRadians(270)) //9 // position
+                .splineToConstantHeading(new Vector2d(50, -57), Math.toRadians(270)) //10 // push
+                //PART 3: pick up specimen
                 .splineToConstantHeading(new Vector2d(43, -65), Math.toRadians(180)) //14 // quarter circle 1
                 .splineToConstantHeading(new Vector2d(38, -63), Math.toRadians(270)) //15 // quarter circle 2
                 .strafeToConstantHeading(new Vector2d(32, -73.5), new TranslationalVelConstraint(10.0))
                 .strafeToConstantHeading(new Vector2d(32, -71), new TranslationalVelConstraint(5.0));
 
-                /*
-                //PART 2c: 3rd push
-                .splineToConstantHeading(new Vector2d(54, -24), Math.toRadians(270)) //12 // position
-                .splineToConstantHeading(new Vector2d(54, -55), Math.toRadians(270)) //13 // push
-
-                //PART 3: pick up 2nd spec
-                .splineToConstantHeading(new Vector2d(48, -60), Math.toRadians(180)) //14 // quarter circle 1
-                .splineToConstantHeading(new Vector2d(38, -67), Math.toRadians(270)); //15 // quarter circle 2
-                */
 
 
 
         TrajectoryActionBuilder score3 = drive.actionBuilder(endZone)
-                //.strafeToConstantHeading(new Vector2d(32, -71), new TranslationalVelConstraint(5.0))
-                .strafeToConstantHeading(new Vector2d(23, -70)) // TODO: change to left. currently 38 maybe 28 instead
+
+                .strafeToConstantHeading(new Vector2d(23, -70))
                 .strafeToLinearHeading(new Vector2d(-5, -42), Math.toRadians(270));
 
         TrajectoryActionBuilder homeStretch = drive.actionBuilder(sub2)
-                .strafeToLinearHeading(new Vector2d(32, -71), Math.toRadians(90))//, new TranslationalVelConstraint(10.0)) //6 // position
-                .strafeToConstantHeading(new Vector2d(32, -74)); //, new TranslationalVelConstraint(10.0));
-                /*.splineToConstantHeading(new Vector2d(39, -57), Math.toRadians(90))
-                .strafeToConstantHeading(new Vector2d(32, -68), new TranslationalVelConstraint(5.0))
-                .strafeToConstantHeading(new Vector2d(32, -73.5), new TranslationalVelConstraint(10.0))
-                .strafeToConstantHeading(new Vector2d(32, -71), new TranslationalVelConstraint(5.0))
-                .strafeToConstantHeading(new Vector2d(20, -70)) // TODO: change to left. currently 38 maybe 28 instead
-                .strafeToLinearHeading(new Vector2d(-5, -41.5), Math.toRadians(270));*/
+                .strafeToLinearHeading(new Vector2d(32, -71), Math.toRadians(90))
+                .strafeToConstantHeading(new Vector2d(32, -74));
+
         TrajectoryActionBuilder done = drive.actionBuilder(finale)
-            .strafeToConstantHeading(new Vector2d(32, -71), new TranslationalVelConstraint(5.0))
-            .strafeToConstantHeading(new Vector2d(20, -70))//, new TranslationalVelConstraint(10.0)) // TODO: change to left. currently 38 maybe 28 instead
-            .strafeToLinearHeading(new Vector2d(-5, -42), Math.toRadians(270));//, new TranslationalVelConstraint(10.0));
+                .strafeToConstantHeading(new Vector2d(32, -71), new TranslationalVelConstraint(5.0))
+                .strafeToConstantHeading(new Vector2d(20, -70))
+                .strafeToLinearHeading(new Vector2d(-5, -42), Math.toRadians(270));
 
 
 
